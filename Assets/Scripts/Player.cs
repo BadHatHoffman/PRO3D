d197832fab01ccf8a1022761b8154b0e3c3cb62c
BadHatHoffman <EHoffman@student.neumont.edu>
2021-06-28 16:55:02 -0600

    Controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public float speed = 2;
     public GameObject shot;
 
-    private Vector2 input;
+    Vector2 input;
 
     void Start()
     {
-        
+        GetComponent<PlayerInput>().onActionTriggered += HandleAction;
     }
 
     void Update()
@@ -21,14 +22,42 @@ public class Player : MonoBehaviour
 
         transform.Translate(input * speed * Time.deltaTime);
 
-        if (Input.GetButtonDown("Fire1")) 
+        Gamepad gamepad = Gamepad.current;
+        if (gamepad == null) return;
+
+        input = gamepad.leftStick.ReadValue();
+
+        if(gamepad.buttonSouth.wasPressedThisFrame)
         {
             OnFire();
         }
     }
 
-    void OnFire()
+    public void OnFire()
     {
         Instantiate(shot, transform.position, Quaternion.identity);
+    }
+
+    public void OnMove(InputValue inputValue)
+    {
+        input = inputValue.Get<Vector2>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+    }
+
+    private void HandleAction(InputAction.CallbackContext context)
+    {
+        if(context.action.name == "Fire")
+        {
+            OnFire();
+        }
+
+        if(context.action.name == "Move")
+        {
+            OnMove(context);
+        }
     }
 }
