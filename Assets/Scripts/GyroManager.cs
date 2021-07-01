d15ede47b0fb7e17e37a5d99c85b88ec3535bfdd
BadHatHoffman <EHoffman@student.neumont.edu>
2021-07-01 09:43:22 -0600

    In Class Work

---
+++ Assets/Scripts/GyroManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GyroManager : MonoBehaviour
+{
+    public delegate void gyroDelegate(Quaternion rotation);
+
+    public event gyroDelegate gyroUpdateEvent;
+
+    GyroControls gyroControls;
+    bool touched = false;
+
+    #region Singleton
+    static GyroManager instance;
+    static public GyroManager Instance
+    {
+        get { if (instance == null) { instance = FindObjectOfType<GyroManager>(); } return instance; }
+    }
+    #endregion
+
+    public Quaternion rotation { get => gyroControls.Gyro.Attitude.ReadValue<Quaternion>(); }
+
+    private void Awake()
+    {
+        gyroControls = new GyroControls();
+    }
+
+    private void OnEnable()
+    {
+        gyroControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        gyroControls.Disable();
+    }
+
+    void Update()
+    {
+        gyroUpdateEvent?.Invoke(rotation);
+    }
+
+}
